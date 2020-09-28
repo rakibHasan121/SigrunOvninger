a50378f625b3f0865f17179e394ca7311d76089d
Rakib Hasan <71521106+rakibHasan121@users.noreply.github.com>
2020-09-28 15:25:07 +0200

    Initial kommit

--- src/Inlämningsuppgift_1/HealtyPets.java
+++ src/Inlämningsuppgift_1/HealtyPets.java
@@ -22,11 +22,11 @@ public class HealtyPets {
             System.exit(0);
         }
         switch (input.toLowerCase()) {
-            case "sixten" -> JOptionPane.showMessageDialog(null, "Hunden Sixten ska få " + String.format("%.1f", Sixten.räknaPortioner()) + "gm av " + Sixten.djurenMatTyp());
-            case "dogge" -> JOptionPane.showMessageDialog(null, "Hunden Dogge ska få " + String.format("%.1f", Dogge.räknaPortioner()) + "gm av " + Dogge.djurenMatTyp());
-            case "venus" -> JOptionPane.showMessageDialog(null, "Katten Venus ska få " + String.format("%.1f", Venus.räknaPortioner()) + "gm av " + Venus.djurenMatTyp());
-            case "ove" -> JOptionPane.showMessageDialog(null, "Katten Ove ska få " + String.format("%.1f", Ove.räknaPortioner()) + "gm av " + Ove.djurenMatTyp());
-            case "hypno" -> JOptionPane.showMessageDialog(null, "Ormen Hypno ska få " + String.format("%.1f", Hypno.räknaPortioner()) + "gm av " + Hypno.djurenMatTyp());
+            case "sixten" -> JOptionPane.showMessageDialog(null, "Hunden Sixten ska få " + String.format("%.1f", Sixten.räknaPortioner()) + " gm av " + Sixten.djurenMatTyp());
+            case "dogge" -> JOptionPane.showMessageDialog(null, "Hunden Dogge ska få " + String.format("%.1f", Dogge.räknaPortioner()) + " gm av " + Dogge.djurenMatTyp());
+            case "venus" -> JOptionPane.showMessageDialog(null, "Katten Venus ska få " + String.format("%.1f", Venus.räknaPortioner()) + " gm av " + Venus.djurenMatTyp());
+            case "ove" -> JOptionPane.showMessageDialog(null, "Katten Ove ska få " + String.format("%.1f", Ove.räknaPortioner()) + " gm av " + Ove.djurenMatTyp());
+            case "hypno" -> JOptionPane.showMessageDialog(null, "Ormen Hypno ska få " + String.format("%.1f", Hypno.räknaPortioner()) + " gm av " + Hypno.djurenMatTyp());
             default -> JOptionPane.showMessageDialog(null, "Matat in felaktiga värden");
         }
     }

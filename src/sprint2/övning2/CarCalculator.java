47698541f18fdbaf4e6f5d6ab2d7697576604efb
Rakib Hasan <71521106+rakibHasan121@users.noreply.github.com>
2020-10-07 02:24:07 +0200

    init commit

--- src/sprint2/övning2/CarCalculator.java
+++ src/sprint2/övning2/CarCalculator.java
@@ -6,16 +6,20 @@ public class CarCalculator {
     protected double mätarställningNu;
     protected double mätarställningFörraÅret;
     protected double bensinFörbrukatsUnderÅret;
-    protected double bensinFörbrukatsFörraÅret;
-    protected double gasUsedLastYear;
 
     public double calculateDistanceDrivenDuringLastYear() {
         return mätarställningNu - mätarställningFörraÅret;
     }
+    public double calculateAverageGasUsage(){
+        return bensinFörbrukatsUnderÅret / calculateDistanceDrivenDuringLastYear();
+    }
     public void run(){
         mätarställningNu= Double.parseDouble(JOptionPane.showInputDialog(null,"Ange mätarställning: ").trim());
         mätarställningFörraÅret= Double.parseDouble(JOptionPane.showInputDialog(null,"Ange mätarställning förra året: ").trim());
-        gasUsedLastYear = Double.parseDouble(JOptionPane.showInputDialog("Ange bensinförbrukning under året: ").trim());
+        bensinFörbrukatsUnderÅret = Double.parseDouble(JOptionPane.showInputDialog("Ange bensinförbrukning under året: ").trim());
+        System.out.println("Antal körda mil: "+calculateDistanceDrivenDuringLastYear());
+        System.out.println("Antal liter bensin: "+bensinFörbrukatsUnderÅret);
+        System.out.println("Förbrukning per mil: "+ calculateAverageGasUsage());
     }
 
     public static void main(String[] args) {

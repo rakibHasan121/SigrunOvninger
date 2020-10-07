0bbe2e576c559088571f843dc12dbb97c3efd948
Rakib Hasan <71521106+rakibHasan121@users.noreply.github.com>
2020-10-07 02:06:17 +0200

    init commit

--- src/sprint2/övning2/CarCalculator.java
+++ src/sprint2/övning2/CarCalculator.java
@@ -1,4 +1,25 @@
 package sprint2.övning2;
 
+import javax.swing.*;
+
 public class CarCalculator {
+    protected double mätarställningNu;
+    protected double mätarställningFörraÅret;
+    protected double bensinFörbrukatsUnderÅret;
+    protected double bensinFörbrukatsFörraÅret;
+    protected double gasUsedLastYear;
+
+    public double calculateDistanceDrivenDuringLastYear() {
+        return mätarställningNu - mätarställningFörraÅret;
+    }
+    public void run(){
+        mätarställningNu= Double.parseDouble(JOptionPane.showInputDialog(null,"Ange mätarställning: ").trim());
+        mätarställningFörraÅret= Double.parseDouble(JOptionPane.showInputDialog(null,"Ange mätarställning förra året: ").trim());
+        gasUsedLastYear = Double.parseDouble(JOptionPane.showInputDialog("Ange bensinförbrukning under året: ").trim());
+    }
+
+    public static void main(String[] args) {
+        CarCalculator cc = new CarCalculator();
+        cc.run();
+    }
 }

7002e93237f984a426ad405c85a7197ef8a5026c
Rakib Hasan <71521106+rakibHasan121@users.noreply.github.com>
2020-10-12 19:24:46 +0200

    init commit

---
+++ src/Inlämningsuppgift/Inlämningsuppgift_2/BestGymEver.java
@@ -0,0 +1,12 @@
+package Inlämningsuppgift.Inlämningsuppgift_2;
+
+
+/**
+ * Created by rakib
+ * Date: 2020-10-12
+ * Time: 17:56
+ * Projekt: SigrunÖvninger
+ * Copyright: MIT
+ */
+public class BestGymEver {
+}

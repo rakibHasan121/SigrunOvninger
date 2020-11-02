c079075b10bf9c66c193c15c5ee72ea0a7ca7010
Rakib Hasan <71521106+rakibHasan121@users.noreply.github.com>
2020-11-02 11:20:38 +0100

    init commit

--- src/sprint4/InetAddressDemo/InetAddressDemo.java
+++ src/sprint4/InetAddressDemo/InetAddressDemo.java
@@ -9,21 +9,21 @@ class InetAddressDemo {
         System.out.println(Address);
         System.out.println(Address.getHostAddress());
         System.out.println(Address.getHostName());
-//        System.out.println(Address.isMulticastAddress());
-////
-////        //Loopback, pekar alltid på den egna datorn
-//        InetAddress loopback = InetAddress.getLoopbackAddress();
-//        System.out.println(loopback);
-//
-//        //IP till dn.se
-//        Address = InetAddress.getByName("dn.se");
-//        System.out.println(Address);
-//
-//        //Ett datornamn kan ha flera IP
-//        InetAddress SW[] = InetAddress.getAllByName("www.hm.se");
-//
-//        for (int i=0; i<SW.length; i++) {
-//            System.out.println(SW[i]);
-//        }
+        System.out.println(Address.isMulticastAddress());
+
+        //Loopback, pekar alltid på den egna datorn
+        InetAddress loopback = InetAddress.getLoopbackAddress();
+        System.out.println(loopback);
+
+        //IP till dn.se
+        Address = InetAddress.getByName("dn.se");
+        System.out.println(Address);
+
+        //Ett datornamn kan ha flera IP
+        InetAddress SW[] = InetAddress.getAllByName("www.hm.se");
+
+        for (int i = 0; i < SW.length; i++) {
+            System.out.println(SW[i]);
+        }
     }
 }
